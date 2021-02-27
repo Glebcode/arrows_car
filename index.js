d8c3bb630057814775ada44ecea51b0c0bc863c5
Gleb Stachev <stachev.gleb@gmail.com>
2021-02-27 11:43:26 +0200

    update(js, css, html):creating content

---
+++ index.js
@@ -0,0 +1,26 @@
+let bodyElement = document.querySelector('body');
+let topM = 0;
+let leftM = 0;
+let rightM = 0;
+let bottomM = 0;
+bodyElement.addEventListener('keydown', event => {
+    console.log(event.keyCode)
+    let car = document.getElementById("car")
+
+    if (event.keyCode == 38) {
+        topM-=20
+        car.setAttribute('style',`transform: matrix(1, 0, 0, 1, ${leftM}, ${topM})` )
+    }
+    if(event.keyCode == 37){
+        leftM-=20;
+        car.setAttribute('style',`transform: matrix(1, 0, 0, 1, ${leftM},  ${topM})` )
+    }
+    if(event.keyCode == 40){
+        topM+=20;
+        car.setAttribute('style',`transform: matrix(1, 0, 0, 1, ${leftM}, ${topM})` )
+    }
+    if(event.keyCode == 39){
+        leftM+=20;
+        car.setAttribute('style',`transform: matrix(1, 0, 0, 1, ${leftM}, ${topM})` )
+    }
+});
