6b705cc98eefbc9475525395c2bba58af2a9f40b
Gleb Stachev <stachev.gleb@gmail.com>
2021-03-06 12:44:20 +0200

    update(js): turning in move

--- index.js
+++ index.js
@@ -5,22 +5,23 @@ let rightM = 0;
 let bottomM = 0;
 bodyElement.addEventListener('keydown', event => {
     console.log(event.keyCode)
-    let car = document.getElementById("car")
+    let car = document.getElementById("img")
 
     if (event.keyCode == 38) {
         topM-=20
-        car.setAttribute('style',`transform: matrix(1, 0, 0, 1, ${leftM}, ${topM})` )
+        car.setAttribute('style',`transform: matrix(1, 0, 0, 1, ${leftM}, ${topM}) rotate(90deg)`)
     }
     if(event.keyCode == 37){
         leftM-=20;
-        car.setAttribute('style',`transform: matrix(1, 0, 0, 1, ${leftM},  ${topM})` )
+        car.setAttribute('style',`transform: matrix(1, 0, 0, 1, ${leftM},  ${topM})`)
     }
     if(event.keyCode == 40){
         topM+=20;
-        car.setAttribute('style',`transform: matrix(1, 0, 0, 1, ${leftM}, ${topM})` )
+        car.setAttribute('style',`transform: matrix(1, 0, 0, 1, ${leftM}, ${topM}) rotate(-90deg)`)
     }
     if(event.keyCode == 39){
         leftM+=20;
-        car.setAttribute('style',`transform: matrix(1, 0, 0, 1, ${leftM}, ${topM})` )
+        car.setAttribute('style',`transform: matrix(1, 0, 0, 1, ${leftM}, ${topM}) rotate(180deg)`)
     }
 });
+ 
